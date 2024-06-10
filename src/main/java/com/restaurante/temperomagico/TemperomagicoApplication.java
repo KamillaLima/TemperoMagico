4c3aa20d6693140949ac01a707e62c20061b852b
Kamilla Lima <kamillalima868@gmail.com>
2024-06-10 18:40:44 -0300

    criacao do projeto

---
+++ src/main/java/com/restaurante/temperomagico/TemperomagicoApplication.java
@@ -0,0 +1,13 @@
+package com.restaurante.temperomagico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperomagicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperomagicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restaurante/temperomagico/TemperomagicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurante.temperomagico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperomagicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
